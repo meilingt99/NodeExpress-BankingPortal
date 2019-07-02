89f172b3a3455aa61563413d9d81ad9548ba9710
Meiling Thompson <meilingthompson@Meilings-MacBook-Pro.local>
2019-07-02 17:25:06 -0400

    Finished module3

--- src/app.js
+++ src/app.js
@@ -5,13 +5,14 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 const accountData = fs.readFileSync(
-  path.join(__dirname, 'json', 'accounts.json'), 'utf-8'
+  path.join(__dirname, 'json', 'accounts.json'), 'utf8'
 );
 const accounts = JSON.parse(accountData);
 
 const userData = fs.readFileSync(
-  path.join(__dirname, 'json', 'users.json'), 'utf-8'
+  path.join(__dirname, 'json', 'users.json'), 'utf8'
 );
 const users = JSON.parse(userData);
 
@@ -36,6 +37,36 @@ app.get('/credit', function(req,res) {
 
 app.get('/profile', function(req,res) {
   res.render('profile', {user: users[0]});
+})
+
+app.get('/transfer', function(req,res) {
+  res.render('transfer');
+})
+
+app.post('/transfer', (req,res) => {
+  accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
+  accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount);
+  let accountsJSON = JSON.stringify(accounts);
+  fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+  res.render('transfer', { message: "Transfer Completed" });
+})
+
+app.get('/payment', (req,res) => {
+  res.render('payment',{
+    account: accounts.credit
+  })
+})
+
+app.post('/payment', (req,res) => {
+  accounts.credit.balance = accounts.credit.balance - req.body.amount;
+  accounts.credit.available = parseInt(accounts.credit.available) + parseInt(req.body.amount);
+  let accountsJSON = JSON.stringify(accounts);
+  fs.writeFileSync(
+    path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+  res.render('payment', {
+    message: "Payment Successful",
+    account: accounts.credit
+  });
 })
 
 app.listen(3000, () => console.log('PS Project Running on port 3000!'))
